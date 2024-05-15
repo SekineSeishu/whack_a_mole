285350f32c4832b70b96d651c268c0c4c1091834
SekineSeishu <mika242401@gmail.com>
2024-05-15 12:03:32 +0900

    スコア、制限時間の追加

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,33 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]private TMP_Text scoreText;
+    [SerializeField] private TMP_Text timeText;
+    private int score;
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        time = 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        time -= Time.deltaTime;
+        scoreText.text = "Score:" + score.ToString();
+        timeText.text = "Time:" + time.ToString();
+        if (time > 0)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo;
-            if (Physics.Raycast(ray, out hitInfo))
+            if (Input.GetMouseButtonDown(0))
             {
-                moleMove moleMove = hitInfo.collider.GetComponent<moleMove>();
-                if (moleMove != null)
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
                 {
-                    if (moleMove._hit)
+                    moleMove moleMove = hitInfo.collider.GetComponent<moleMove>();
+                    if (moleMove != null)
                     {
-                        moleMove.OnHit();
+                        if (moleMove._hit)
+                        {
+                            score++;
+                            moleMove.OnHit();
+                        }
                     }
                 }
             }
+        }
+        else
+        {
+            Time.timeScale = 0f;
         }
     }
 }
